cccd16936d53dfdd65050bb719a0b782de1a7e4a
cstudio7 <41308548+cstudio7@users.noreply.github.com>
2019-06-08 05:53:29 -1200

    Update order.model.js

--- models/order.model.js
+++ models/order.model.js
@@ -1,205 +1,56 @@
-const Promise = require("promise");
-const helper = require("../helpers/helper.js");
+const express = require("express");
+const router = express.Router();
+const order = require("../models/order.model");
+const m = require("../helpers/middlewares");
 
-let orders = [
-    {
-        //dummy values for testing
-        "id": 1,
-        "buyer": 1, // user id
-        "car_id": 1, // car id
-        "amount": 82636.62,  // price of car
-        "status": "pending",  // pending, accepted or rejected
-        "date": helper.newDate()
-    }
-];
-
-
-const getOrders = () => {
-    return new Promise((resolve, reject) => {
-
-        if (orders.length === 0) {
-            reject({
-                message: "No Orders Available",
-                status: 202
+/* All Orders */
+router.get("/", async(req, res) => {
+    await order.getOrders()
+        .then(order => {
+            res.status(200).json({
+                status: 200,
+                data: order
             })
-        }
+        })
+        .catch(err => {
+            res.status(202).json({
+                status: 202,
+                error: {
+                    message: err.message
+                }
+            })
+        })
+});
 
-        resolve(orders);
-    })
-};
 
-//
-// const updateStatus = (id, status) => {
-//     return new Promise((resolve, reject) => {
-//         helper.mustBeInArray(cars, id)
-//             .then(car => {
-//
-//                 const index = cars.findIndex(p => p.id === car.id);
-//
-//                 const id = {
-//                     id: car.id
-//                 };
-//
-//                 const owner = {
-//                     owner: car.owner
-//                 };
-//
-//                 const date = {
-//                     created_on: car.created_on,
-//                     updated_on: helper.newDate()
-//                 };
-//
-//                 car.status = status;
-//
-//                 cars[index] = {
-//                     ...id,
-//                     ...owner,
-//                     ...date,
-//                     ...car
-//                 };
-//
-//                 resolve(cars[index]);
-//             })
-//             .catch(err => reject(err))
-//     })
-// };
-//
-//
-// const viewAllUnsold = (status) => {
-//     return new Promise((resolve, reject) => {
-//         helper.findAvailableCarsInArray(cars, status)
-//             .then(car => {
-//                 resolve(car);
-//             })
-//             .catch(err => reject(err))
-//     });
-// };
-//
-// const viewAllUnsoldWithinAPriceRange = (status, min_price, max_price) => {
-//     return new Promise((resolve, reject) => {
-//         helper.findAvailableCarsInArray(cars, status, min_price, max_price)
-//             .then(car => {
-//                 resolve(car);
-//             })
-//             .catch(err => reject(err))
-//     });
-// };
-//
-//
-// const updatePrice = (id, price) => {
-//     return new Promise((resolve, reject) => {
-//         helper.mustBeInArray(cars, id)
-//             .then(car => {
-//
-//                 const index = cars.findIndex(p => p.id === car.id);
-//
-//                 const id = {
-//                     id: car.id
-//                 };
-//
-//                 const owner = {
-//                     owner: car.owner
-//                 };
-//
-//                 const date = {
-//                     created_on: car.created_on,
-//                     updated_on: helper.newDate()
-//                 };
-//
-//                 car.price = helper.currencyFormatter().format(price);
-//
-//                 cars[index] = {
-//                     ...id,
-//                     ...owner,
-//                     ...date,
-//                     ...car
-//                 };
-//
-//                 resolve(cars[index]);
-//             })
-//             .catch(err => reject(err))
-//     })
-// };
-//
-//
-// const getCar = (id) => {
-//     return new Promise((resolve, reject) => {
-//         helper.mustBeInArray(cars, id)
-//             .then(car => resolve(car))
-//             .catch(err => reject(err))
-//     })
-// };
-//
-//
-// const insertCar = (newCar) => {
-//     return new Promise((resolve, reject) => {
-//
-//         const id = {
-//             id: helper.getNewId(cars)  // check latest id in this array and +1 to get new ID
-//         };
-//
-//         const date = {
-//             created_on: helper.newDate()
-//         };
-//
-//         newCar = { ...id, ...date, ...newCar };
-//
-//         cars.push(newCar);
-//
-//         resolve(newCar);
-//     })
-// };
-//
-//
-// const updateCar = (id, newCar) => {
-//
-//     return new Promise((resolve, reject) => {
-//         helper.mustBeInArray(cars, id)
-//             .then(car => {
-//
-//                 const index = cars.findIndex(p => p.id === car.id);
-//                 id = { id: car.id };
-//
-//                 const date = {
-//                     created_on: car.created_on,
-//                     updated_on: helper.newDate()
-//                 };
-//
-//                 cars[index] = { ...id, ...date, ...newCar };
-//
-//                 // helper.writeJSONFile(filename, cars);
-//
-//                 resolve(cars[index]);
-//             })
-//             .catch(err => reject(err))
-//     })
-// };
-//
-//
-// const deleteCar = (id) => {
-//     return new Promise((resolve, reject) => {
-//         helper.mustBeInArray(cars, id)
-//             .then(() => {
-//
-//                 cars = cars.filter(p => parseInt(p.id) !== parseInt(id));
-//
-//                 resolve();
-//
-//             })
-//             .catch(err => reject(err))
-//     })
-// };
+/* Create a car order */
+router.post("/", m.checkFieldPostOrder, async(req, res) => {
+    await order.createOrder(req.body)
+        .then(order => res.status(201).json({
+            status: 201,
+            data: order
+        }))
+        .catch(err => res.status(500).json({
+            message: err.message
+        }))
+});
+
+// update the price of a purchase order
+router.patch('/:id/price', m.mustBeInteger, async (req, res) => {
+    const id = req.params.id;
+    
+    const price = req.body.price;
+
+    await order.updatePrice(id, price)
+    .then(order => res.json({
+        price: 202,
+        data: order
+    }))
+    .catch()
+});
 
 
 
-module.exports = {
-    getOrders,
-    // getCars,
-    // insertCar,
-    // updateCar,
-    // updateStatus,
-    // updatePrice,
-    // viewAllUnsold,
-    // viewAllUnsoldWithinAPriceRange,
-    // deleteCar
-};
+
+// Routes
+module.exports = router;
