04b61c8745a4cb52c45d24dd98cfa498e7928f63
cstudio7 <41308548+cstudio7@users.noreply.github.com>
2019-06-07 20:59:25 -1200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const Promise = require("promise");
 const cors = require("cors");
+const swaggerUi = require('swagger-ui-express');
 
 // App calling
 const app = new express();
