c8e56a6612a1a4b7b9c4ae3ba8a45f8aeef7b614
cstudio7 <41308548+cstudio7@users.noreply.github.com>
2019-06-08 06:16:24 -1200

    Update helper.js

--- helpers/helper.js
+++ helpers/helper.js
@@ -1,114 +1,125 @@
-const fs = require('fs');
-const Promise = require('promise');
+const fs = require("fs");
+const Promise = require("promise");
 
 
 // token generator using hasha
-const hasha = require('hasha');
+const hasha = require("hasha");
 
 // bcrypt password hashing
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 const passwordHash = (password) => {
-  const saltRounds = 10;
-  return bcrypt.hashSync(password, saltRounds);
+    let saltRounds = 10;
+    return bcrypt.hashSync(password, saltRounds);
 };
 
 const passwordCompare = async (password, hash) => {
-  const match = await bcrypt.compare(password, hash);
-  if (match) {
-    return true;
-  }
+    const match = await bcrypt.compare(password, hash);
+    if (match) {
+        return true;
+    }
 };
+
 
 // Increment ID automatically
 const getNewId = (array) => {
-  if (array.length > 0) {
-    return array[array.length - 1].id + 1;
-  }
-  return 1;
+    if (array.length > 0) {
+        return array[array.length - 1].id + 1;
+    } else {
+        return 1
+    }
 };
 
 // Create new date automatically
 const newDate = () => new Date().toString();
 
 // Check if ID is in data store
-const mustBeInArray = (array, id) => new Promise((resolve, reject) => {
-  const row = array.find(r => r.id == id);
+const mustBeInArray = (array, id) => {
+    return new Promise((resolve, reject) => {
+        const row = array.find(r => r.id == id);
+        if (!row) {
+            reject({
+                message: `The id ${id} does not exist in the database`,
+                status: 404
+            })
+        }
+        resolve(row);
+    })
+};
 
-  if (!row) {
-    reject({
-      message: `The id ${id} does not exist in the database`,
-      status: 404,
+const emailExistsInArray = (array, email, password) => {
+    return new Promise((resolve, reject) => {
+
+        if (email && password) {
+
+            const row = array.filter(r => r.email === email && r.password === password);
+
+            if (row.length === 0) {
+                reject({
+                    message: `The user does not exist in the database`,
+                    status: 404
+                })
+            }
+
+            resolve(row);
+        }
+
     });
-  }
-  resolve(row);
-});
-
-
-const findAvailableCarsInArray = (array, status, min_price, max_price) => new Promise((resolve, reject) => {
-  if (status && !min_price && !max_price) {
-    const row = array.filter(r => r.status == status);
-
-    if (row.length === 0) {
-      reject({
-        message: 'No cars available. All cars have been sold.',
-        status: 404,
-      });
-    }
-
-    resolve(row);
-  } else if (status && min_price && max_price) {
-    const row = array.filter(r => r.status == status && r.price >= min_price && r.price <= max_price);
-
-    if (row.length === 0) {
-      reject({
-        message: `No cars available within these price range of ${min_price} and ${max_price}`,
-        status: 404,
-      });
-    }
-
-    resolve(row);
-  }
-});
-
-
-const emailExistsInArray = (array, email, password) => new Promise((resolve, reject) => {
-  if (email && password) {
-    const row = array.filter(r => r.email === email && r.password === password);
-
-    if (row.length === 0) {
-      reject({
-        message: 'The user does not exist in the database',
-        status: 404,
-      });
-    }
-
-    resolve(row);
-  }
-});
-
+};
 
 // Format currency
 const currencyFormatter = () => new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
-  minimumFractionDigits: 2,
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2
 });
 
 const tokenGenerator = (text) => {
-  const token = hasha(text);
-  return token.slice(0, 40);
+    const token = hasha(text);
+    return token.slice(0, 40);
 };
+const findAvailableCarsInArray = (array, status, min_price, max_price) => {
+    return new Promise((resolve, reject) => {
+
+        if (status && !min_price && !max_price) {
+
+            const row = array.filter(r => r.status == status);
+
+            if (row.length === 0) {
+                reject({
+                    message: `No cars available. All cars have been sold.`,
+                    status: 404
+                });
+            }
+
+            resolve(row);
+
+        } else if (status && min_price && max_price) {
+
+            let row = array.filter(r => r.status == status && r.price >= min_price && r.price <= max_price);
+
+            if (row.length === 0) {
+                reject({
+                    message: `No cars available within these price range of ${min_price} and ${max_price}`,
+                    status: 404
+                });
+            }
+
+            resolve(row);
+        }
+    });
+};
+
 
 // Export everything for use elsewhere
 module.exports = {
-  getNewId,
-  newDate,
-  mustBeInArray,
-  currencyFormatter,
-  passwordHash,
-  passwordCompare,
-  tokenGenerator,
-  emailExistsInArray,
-  findAvailableCarsInArray,
+    getNewId,
+    newDate,
+    mustBeInArray,
+    currencyFormatter,
+    passwordCompare,
+    passwordHash,
+    tokenGenerator,
+    emailExistsInArray,
+    findAvailableCarsInArray
 };
