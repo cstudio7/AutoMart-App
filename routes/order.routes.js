c57af3e4f4bbb9a5a22b497d1aaff6014cc4bb1a
cstudio7 <41308548+cstudio7@users.noreply.github.com>
2019-06-08 05:57:52 -1200

    Update order.routes.js

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,34 +1,35 @@
-const express = require("express");
-const router = express.Router();
-const order = require("../models/order.model");
-const m = require("../helpers/middlewares");
+const express = require('express');
 
-router.get("/", async(req, res) => {
-    await order.getOrders()
-        .then(order => {
-            res.status(200).json({
-                status: 200,
-                data: order
-            })
-        })
-        .catch(err => {
-            res.status(202).json({
-                status: 202,
-                error: {
-                    message: err.message
-                }
-            })
-        })
+const router = express.Router();
+const path = require('path');
+const order = require('../models/order.model');
+const m = require('../helpers/middlewares');
+
+/** ***All Orders**** */
+router.get('/', async (req, res) => {
+  await order.getOrders()
+    .then((order) => {
+      res.status(200).json({
+        status: 200,
+        data: order,
+      });
+    })
+    .catch((err) => {
+      res.status(202).json({
+        status: 202,
+        error: {
+          message: err.message,
+        },
+      });
+    });
 });
 
-
-
-/* Insert a new order */
-router.post("/", m.checkFieldPostCar, async(req, res) => {
-    await car.insertCar(req.body)
-        .then(car => res.status(201).json({
-            message: `Car #${car.id} has been created`,
-            content: car
+/* Create a car order */
+router.post("/", m.checkFieldPostOrder, async(req, res) => {
+    await order.createOrder(req.body)
+        .then(order => res.status(201).json({
+            status: 201,
+            data: order
         }))
         .catch(err => res.status(500).json({
             message: err.message
@@ -37,8 +38,19 @@ router.post("/", m.checkFieldPostCar, async(req, res) => {
 
 
 
+// update the price of a purchase order
+router.patch('/:id/price', m.mustBeInteger, async (req, res) => {
+  const { id } = req.params;
 
+  const { price } = req.body;
 
+  await order.updatePrice(id, price)
+    .then(order => res.json({
+      price: 202,
+      data: order,
+    }))
+    .catch();
+});
 
 
 
