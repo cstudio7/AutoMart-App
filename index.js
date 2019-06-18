faedf1bc9a9e3177eece57bf81685fe6f228d885
cstudio7 <41308548+cstudio7@users.noreply.github.com>
2019-06-17 20:33:26 -1200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const morgan = require("morgan");
 const Promise = require("promise");
 const cors = require("cors");
-
 // App calling
 const app = new express();
 const PORT = process.env.PORT || 3000;
@@ -16,5 +15,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(require('./routes/index.routes'));
 app.use(cors());
 
+// when a random route is inputed
+app.get('*', (req, res) => res.status(200).send({
+   message: 'Welcome to Victor Godwin API.'
+}));
 // Port listen
 module.exports = app.listen(PORT);
