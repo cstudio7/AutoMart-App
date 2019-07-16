893bfd9648e8c124e4feca27923226bb0891f0ec
cstudio7 <41308548+cstudio7@users.noreply.github.com>
2019-07-15 14:06:57 -1200

    Update car.routes.js

--- routes/car.routes.js
+++ routes/car.routes.js
@@ -1,144 +1,200 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken')
+const express = require("express");
 const router = express.Router();
-const user = require('../models/user.model');
-const m = require('../helpers/middlewares');
+const car = require("../models/car.model");
+const m = require("../helpers/middlewares");
+const helper = require("../helpers/helper");
+const path = require('path');
 const db = require('../db');
 
 
+const joi = require('@hapi/joi');
 
-
-/* Create user account * Andela */
-router.post('/auth/signup',  async (req, res) => {
-  const {email, name, password } = req.body;
-  // const hash = bcrypt.hashSync(password);
-  const saltRounds = 10;
-
-  bcrypt.hash(password, saltRounds, function(err, hash) {
-    db('login').insert({
-      email: email,
-      password: hash
+/* Mark a posted car Ad as sold * Andela */
+router.patch("/:id/status", m.mustBeInteger, async(req, res) => {
+     const id = req.params.id;
+     const status = req.body;
+    // postgres db
+    db('cars')
+    .where({
+        'id' : id
     })
-    return  res.json({
-      status: 201,
-      data: req.body
-    })
+    .update(status)
+    .returning('*')
+    //    then()
+    .then(car => res.status(202).json({
+        status: 202,
+        data: car
+    }))
+    // .catch()
     .catch(err => res.status(500).json({
-      status: 500,
-      error: {
-        message: err.message,
-      },
-    }));
-  });
-
-});
-  //       .into('login')
-  //       .returnung('email')
-  //       .then(loginEmail => {
-  //         return trx('users')
-  //             .returning('*')
-  //             .insert(req.body)
-  //             .then(user => {
-  //               res.json(user[0]);
-  //             })
-  //       })
-
-
-
-/* Login a user * Andela */
-router.post('/auth/signin', m.checkLoginForm, async (req, res) => {
-  const { email } = req.body;
-  const { password } = req.body;
-
-  await user.loginUser(email, password)
-    .then(user => res.status(200).json({
-      status: 200,
-      data: user,
+        message: err.message
     }))
-    .catch((err) => {
-      res.status(401).json({
-        status: 401,
-        error: {
-          message: 'Invalid id and password',
-        },
-      });
-    });
 });
 
 
-/* Update a user */
-router.put('/:id', m.mustBeInteger, m.checkFieldPostUser, async (req, res) => {
-  const { id } = req.params;
+/* Update the price of a car * Andela */
+router.patch("/:id/price", m.mustBeInteger, async(req, res) => {
+    const id = req.params.id;
+    const price = req.body;
 
-  await user.updateUser(id, req.body)
-    .then(user => res.json({
-      message: `User #${id} has been updated`,
-      content: user,
-    }))
-    .catch((err) => {
-      if (err.status) {
-        res.status(err.status).json({
-          message: err.message,
-        });
-      }
-      res.status(500).json({
-        message: err.message,
-      });
-    });
+    // postgres db
+    db('cars')
+        .where({
+            'id' : id
+        })
+        .update(price)
+        .returning('*')
+        //    then function
+        .then(car => res.status(202).json({
+            status: 202,
+            data: car
+        }))
+        // .catch()
+        .catch(err => res.status(500).json({
+            message: err.message
+        }))
+});
+
+// /* Insert a new car */
+router.post('/', m.checkFieldPostCar, async(req, res) => {
+        db('cars')
+        .insert(req.body)
+        .returning('*')
+        .then(cars => res.status(201).json({
+             message: `car  has been created`,
+             content: cars
+         }))
+        .catch(err => res.status(500).json({
+            message: err.message
+        }))
 });
 
 
-/* Delete a user */
+/* Delete a car */
 router.delete('/:id', m.mustBeInteger, async (req, res) => {
-  const { id } = req.params;
-
-  await user.deleteUser(id)
-    .then(user => res.json({
-      message: `User #${id} has been deleted`,
-      status: 'OK',
-    }))
-    .catch((err) => {
-      if (err.status) {
-        res.status(err.status).json({
-          message: err.message,
-        });
-      }
-      res.status(500).json({
-        message: err.message,
-      });
-    });
+    const id = req.params.id;
+        db('cars')
+        .where('id',id)
+        .del()
+        .then(car => res.json({
+            message: `The car #${id} has been deleted`,
+            status: "Car Ad successfully deleted"
+        }))
+        .catch(err => {
+            if (err.status) {
+                res.status(err.status).json({
+                    message: err.message
+                })
+            }
+            res.status(500).json({
+                message: err.message
+            })
+        })
 });
 
-// new
-router.post('/signup', (req, res) => {
-  bcrypt.hash(req.body.password, 10, function(err, hash){
-    if(err) {
-      return res.status(500).json({
-        error: err
-      });
-    }
-    else {
-      // const user = new User({
-      //   _id: new  mongoose.Types.ObjectId(),
-      //   email: req.body.email,
-      //   password: hash
-      // });
-      // user.save()
-      db('users').insert(req.body)
-        .then(function(result) {
-        console.log(result);
-        res.status(200).json({
-          success: 'New user has been created'
-        });
-      }).catch(error => {
-        res.status(500).json({
-          error: err
-        });
-      });
-    }
-  });
+/* Update a car */
+router.put('/:id', m.mustBeInteger, m.checkFieldPostCar, async (req, res) => {
+
+    const id = req.params.id;
+
+    await car.updateCar(id, req.body)
+        .then(car => res.json({
+            message: `Car #${id} has been updated`,
+            content: car
+        }))
+        .catch(err => {
+            if (err.status) {
+                res.status(err.status).json({
+                    message: err.message
+                })
+            }
+            res.status(500).json({
+                message: err.message
+            })
+        })
 });
+
+/* View a specific car * Andela */
+router.get("/:id", m.mustBeInteger, async(req, res) => {
+
+    const id = req.params.id;
+    db.select('*')
+        .from('cars')
+        .where({
+            id:id,
+         })
+        .select('id')
+        .then(car => res.json(car))
+        .catch(err => {
+            if (err.status) {
+                res.status(err.status).json({
+                    message: err.message
+                })
+            } else {
+                res.status(500).json({
+                    message: err.message
+                })
+            }
+        })
+});
+
+/* All cars */
+router.get("/", async(req, res) => {
+
+    const status = req.query.status;
+    const min_price = req.query.min_price;
+    const max_price = req.query.max_price;
+
+
+        /* View all unsold cars within a price range * Andela */
+
+        if ( status === "available" && min_price && max_price) {
+            const subquery = db('cars').where('price', '>', 10)
+                .andWhere('price', '<','100').select('id');
+
+            await db('cars').where('id', 'in', subquery)
+                .andWhere('status', 'available')
+                .then(subquery => res.status(200).json({
+                    status: 200,
+                    data: subquery
+                }))
+                .catch(err => res.status(500).json({
+                    message: err.message
+                }))
+
+        } else {
+
+            /* View all unsold cars * Andela */
+
+           await db('cars').where('status', 'available')
+                .then(car => res.status(200).json({
+                    status: 200,
+                    data: car
+                }))
+                .catch(err => res.status(500).json({
+                    message: err.message
+                }))
+        }
+
+
+
+});
+
+
+
+
+/* All cars */
+router.get("/", async(req, res) => {
+    db.select('*').from('cars')
+        .then(car => res.status(200).json({
+            status: 200,
+            data: car
+        }))
+        .catch(err => res.status(500).json({
+            message: err.message
+        }))
+});
+
 
 // Routes
 module.exports = router;
